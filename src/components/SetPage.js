751f3138a76222c3b43dbe73d6e8aea4262e69a6
SavagePixie <43502485+SavagePixie@users.noreply.github.com>
2019-12-29 10:32:05 +0100

    Abstracted setting options into Setting component

--- src/components/SetPage.js
+++ src/components/SetPage.js
@@ -29,60 +29,42 @@ const SetPage = props => (
                 numberOfLines={3}
             />
 
-            <View style={styles.picker}>
-                <Text style={styles.label}>Repeat</Text>
-                <View style={styles.pickerOption}>
-                    <Picker
-                        style={{ ...styles.label, flex: 1, marginLeft: 40 }}
-                        selectedValue={props.memo.repeat.value}
-                        onValueChange={props.repeatFunc}
-                    >
-                        {repeatOptions.map(x => <Picker.Item label={x.key} value={x.value} key={`repeat-${x.value}`} />)}
-                    </Picker>
-                </View>
-            </View>
-            {props.memo.repeat.value === 'time' && <View style={styles.picker}>
-                <Text style={styles.label}>Every</Text>
-                <View style={styles.pickerOption}>
-                    <TextInput
-                        style={{ ...styles.bold, ...styles.label, flex: 1, textAlign: 'center' }}
-                        onChangeText={props.repeatTimeFunc}
-                        value={props.memo.repeatTime.toString()}
-                        keyboardType='numeric'
-                    />
-                    <Text style={{ ...styles.label, marginRight: 25 }}>{props.memo.repeatTime == 1 ? 'hour' : 'hours'}</Text>
-                </View>
-            </View>}
+            <Setting label='Repeat'>
+                <Picker
+                    style={{ ...styles.label, flex: 1, marginLeft: 40 }}
+                    selectedValue={props.memo.repeat.value}
+                    onValueChange={props.repeatFunc}
+                >
+                    {repeatOptions.map(x => <Picker.Item label={x.key} value={x.value} key={`repeat-${x.value}`} />)}
+                </Picker>
+            </Setting>
+            {props.memo.repeat.value === 'time' && <Setting label='Every'>
+                <TextInput
+                    style={{ ...styles.bold, ...styles.label, flex: 1, textAlign: 'center' }}
+                    onChangeText={props.repeatTimeFunc}
+                    value={props.memo.repeatTime.toString()}
+                    keyboardType='numeric'
+                />
+                <Text style={{ ...styles.label, marginRight: 25 }}>{props.memo.repeatTime == 1 ? 'hour' : 'hours'}</Text>
+            </Setting>}
 
-            <View style={styles.picker}>
-                <Text style={styles.label}>Date</Text>
-                <View style={styles.pickerOption}>
-                    {/*<TouchableOpacity style={styles.touchOption} onPress={props.dateFunc}>
-                        <Text style={{ ...styles.bold, ...styles.label }}>{props.memo.date}</Text>
-</TouchableOpacity>*/}
-                    <Button
-                        func={props.dateFunc}
-                        text={props.memo.date}
-                        textStyle={{ ...styles.bold, ...styles.label }}
-                        touchStyle={styles.touchOption}
-                    />
-                </View>
-            </View>
+            <Setting label='Date'>
+                <Button
+                    func={props.dateFunc}
+                    text={props.memo.date}
+                    textStyle={{ ...styles.bold, ...styles.label }}
+                    touchStyle={styles.touchOption}
+                />
+            </Setting>
 
-            <View style={styles.picker}>
-                <Text style={styles.label}>Time</Text>
-                <View style={styles.pickerOption}>
-                    {/*<TouchableOpacity style={styles.touchOption} onPress={props.timeFunc}>
-                        <Text style={{ ...styles.bold, ...styles.label }}>{props.memo.time}</Text>
-</TouchableOpacity>*/}
-                    <Button
-                        func={props.timeFunc}
-                        text={props.memo.time}
-                        textStyle={{ ...styles.bold, ...styles.label }}
-                        touchStyle={styles.touchOption}
-                    />
-                </View>
-            </View>
+            <Setting label='Time'>
+                <Button
+                    func={props.timeFunc}
+                    text={props.memo.time}
+                    textStyle={{ ...styles.bold, ...styles.label }}
+                    touchStyle={styles.touchOption}
+                />
+            </Setting>
         </ScrollView>
         <View style={styles.group}>
             <Button
@@ -105,4 +87,13 @@ const SetPage = props => (
             />
         </View>
     </FadeInView>
+)
+
+const Setting = props => (
+    <View style={styles.picker}>
+        <Text style={styles.label}>{props.label}</Text>
+        <View style={styles.pickerOption}>
+            {props.children}
+        </View>
+    </View>
 )
