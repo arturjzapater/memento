19c9bff7461a85b5d2edcc1ca8b7ab36f763b916
SavagePixie <43502485+SavagePixie@users.noreply.github.com>
2020-01-02 15:42:45 +0100

    fixed indentation

--- src/NotifService.js
+++ src/NotifService.js
@@ -3,73 +3,73 @@ import PushNotification from 'react-native-push-notification'
 import { clearToDelete, find, remove, removeAll, update } from './StoreService'
 
 PushNotification.configure({
-  smallIcon: 'ic_notification',
-  onNotification: notification => Alert.alert(
-      notification.title,
-      notification.message,
-      [
-        {
-          text: 'Snooze',
-          onPress: () => snoozeNotif(notification.title, notification.message),
-        },
-        {
-          text: 'Dismiss',
-        },
-      ]
-    ),
-  senderID: '',
-  popInitialNotification: true,
-  requestPermissions: true,
+	smallIcon: 'ic_notification',
+	onNotification: notification => Alert.alert(
+		notification.title,
+		notification.message,
+		[
+			{
+				text: 'Snooze',
+				onPress: () => snoozeNotif(notification.title, notification.message),
+			},
+			{
+				text: 'Dismiss',
+			},
+		]
+	),
+	senderID: '',
+	popInitialNotification: true,
+	requestPermissions: true,
 })
 
 export const cancelAllNotifs = () => {
-  PushNotification.cancelAllLocalNotifications()
-  return removeAll()
+	PushNotification.cancelAllLocalNotifications()
+	return removeAll()
 }
 
 export const cancelNotif = id => {
-  PushNotification.cancelLocalNotifications({ id: id.toString() })
-  return remove(id)
+	PushNotification.cancelLocalNotifications({ id: id.toString() })
+	return remove(id)
 }
 
 export const removeDeleted = () => find('@to-delete')
-  .then(data => data.forEach(x => PushNotification.cancelLocalNotifications({ id: x.id.toString() })))
-  .then(() => clearToDelete())
+	.then(data => data.forEach(x => PushNotification.cancelLocalNotifications({ id: x.id.toString() })))
+	.then(() => clearToDelete())
 
 export const scheduleNotif = (info) => getLastId()
-    .then(lastId => {
-      const notification = {
-        ...info,
-        id: lastId + 1,
-        repeatTime: info.repeatType == 'time' ? info.repeatTime * 3600000 : undefined
-      }
-      pushNotif(notification)
-      return(notification)
-    })
-    .then(data => update(data))
+	.then(lastId => {
+		const notification = {
+			...info,
+			id: lastId + 1,
+			repeatTime: info.repeatType == 'time' ? info.repeatTime * 3600000 : undefined
+		}
+		pushNotif(notification)
+		return(notification)
+	})
+	.then(data => update(data))
 
 const getLastId = () => find()
-  .then(data => data.reduce((a, b) => b.id > a ? b.id : a, 0))
+	.then(data => data.reduce((a, b) => b.id > a ? b.id : a, 0))
 
 const pushNotif = ({ id, title, text, date, repeatType, repeatTime }) =>
-  PushNotification.localNotificationSchedule({
-    date: new Date(date),
-    id: id.toString(),
-    ticker: 'Memento Notification Ticker',
-    autoCancel: true,
-    largeIcon: 'ic_launcher',
-    smallIcon: 'ic_notification',
-    subText: 'Remember, remember!',
-    vibrate: true,
-    vibration: 1000,
-    ongoing: true,
-    title: title,
-    message: text,
-    playSound: true,
-    soundName: 'default',
-    repeatType: repeatType,
-    repeatTime: repeatTime,
-  })
+	PushNotification.localNotificationSchedule({
+		date: new Date(date),
+		id: id.toString(),
+		ticker: 'Memento Notification Ticker',
+		autoCancel: true,
+		largeIcon: 'ic_launcher',
+		smallIcon: 'ic_notification',
+		subText: 'Remember, remember!',
+		vibrate: true,
+		vibration: 1000,
+		ongoing: true,
+		title: title,
+		message: text,
+		playSound: true,
+		soundName: 'default',
+		repeatType: repeatType,
+		repeatTime: repeatTime,
+	})
 
 const snoozeNotif = (title, text) => getLastId()
-  .then(id => pushNotif({ id: id + 1, title, text, date: new Date(Date.now() + 600000) }))
+	.then(id => pushNotif({ id: id + 1, title, text, date: new Date(Date.now() + 600000) }))
